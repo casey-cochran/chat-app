051445ed5be6dedeb14e03ef08588c3a4dbc5b87
Casey Cochran <Casey3362@yahoo.com>
2022-08-21 19:00:23 -0700

    user schema created

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
-
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.json({msg: "api working"})
---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export default mongoose.model('User', UserSchema);
