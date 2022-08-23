953e91d22b9678a90c9d05dc4170444c3acc5c65
Casey Cochran <Casey3362@yahoo.com>
2022-08-23 15:35:36 -0700

    login route for user created and successfuly sending back token for known user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -58,7 +58,41 @@ router.post('/signup', validateUser, asyncHandler(async(req,res) => {
     )
 }))
 
+const validateLogin = [
+    check('email', 'Please enter a valid email')
+        .isEmail(),
+    check('password', 'Please enter a valid password')
+        .isLength({min: 6}),
+    handleValidationErrors
+];
 
+router.post('/login', validateLogin, asyncHandler(async(req, res) => {
+    const {email, password} = req.body;
+    let user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({msg: "User Does Not Exist"})
+    }
 
+    const userMatch = await bcrypt.compare(password, user.password);
+    if(!userMatch){
+        return res.status(400).json({msg: "Incorrect Password !"})
+    }
+
+    const payload = {
+        user:{
+            id: user.id
+        }
+    }
+
+    jwt.sign(
+        payload,
+        "randomstring",
+        {expiresIn: 3600},
+        (err, token) => {
+            if(err) throw err;
+            res.status(200).json({token})
+        }
+    )
+}))
 
 export default router;
