d0e2e702c06c8d9e3aab383b319062309806e19c
Casey Cochran <Casey3362@yahoo.com>
2022-08-28 11:40:27 -0700

    generic error handlers added to express app

--- backend/index.js
+++ backend/index.js
@@ -49,17 +49,35 @@ app.use(express.static(path.resolve('../frontend/build')));
 
 
 app.use(function (err, req, res, next) {
+    //error handler for csurf from offical docs
     if (err.code !== 'EBADCSRFTOKEN') return next(err)
-
     // handle CSRF token errors here
     res.status(403)
     res.send('form tampered with')
   })
 
+app.use((req, res, next) => {
+    //error handler to capture all unhandled requests
+    const err = new Error('Requested resource could not be found')
+    err.title = 'Resource Not Found';
+    err.errors = ['Requested resource could not be found'];
+    err.status = 404;
+    next(err)
+});
+
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
+    //error handler to format errors as they are sent back to the front end
+    //show stack if in development
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+  });
+
 
 app.listen(PORT, (req,res) => console.log(`server started on ${PORT}`))
