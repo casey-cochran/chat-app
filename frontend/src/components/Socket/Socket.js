1393d89ca2b2c2b46800adc03c41b8da10450754
Casey Cochran <Casey3362@yahoo.com>
2022-08-29 15:58:22 -0700

    correct data showinginside usestate

--- frontend/src/components/Socket/Socket.js
+++ frontend/src/components/Socket/Socket.js
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
 import {io} from 'socket.io-client';
-
 let socket;
 
 const Socket = () => {
     const [messages, setMessages] = useState([]);
     const [chatInput, setChatInput] = useState('');
-
+    console.log(messages)
     const sendChat = (e) => {
         e.preventDefault();
         socket.emit('chat', {msg: chatInput});
@@ -14,21 +13,22 @@ const Socket = () => {
     }
 
     useEffect(() => {
-        socket = io();
+       socket = io();
 
-        socket.on('chat', (chat) => {
+        socket.on('recieved', (chat) => {
+            // alert('data recieved')
             setMessages(messages => [...messages, chat]);
         })
 
         return (() => socket.disconnect())
-    })
+    }, [socket])
 
     return (
         <div>
             <form onSubmit={sendChat}>
                 <input
                     value={chatInput}
-                    onChange={setChatInput}
+                    onChange={(e) => setChatInput(e.target.value)}
                 />
                 <button type='submit'>Send</button>
             </form>
