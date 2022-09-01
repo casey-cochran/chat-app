860ac84704154763a1dd36ce550ac953c1f951aa
Casey Cochran <Casey3362@yahoo.com>
2022-08-31 21:48:45 -0700

    testing reactstrap as alternative to tailwnid

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { csrfFetch } from "../../store/csrf";
+import { Form, FormGroup, Label, Input, Container, Col, Row } from "reactstrap";
 
 const Signup = () => {
   const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const user = {
@@ -26,25 +27,35 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div >
+      <Container>
+        <Row>
+        <Col></Col>
+        <Col>
+      <Form className=".col-6 .col-sm-4"  onSubmit={handleSubmit}>
+        <FormGroup>
         <div>
-          <label>Username</label>
-          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+          <Label>Username</Label>
+          <Input type="text" onChange={(e) => setUserName(e.target.value)} />
         </div>
         <div>
-          <label>Email</label>
-          <input type="text" onChange={(e) => setEmail(e.target.value)} />
+          <Label>Email</Label>
+          <Input type="text" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
-          <label>Password</label>
-          <input
+          <Label>Password</Label>
+          <Input
             type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <button>Submit</button>
-      </form>
+        </FormGroup>
+      </Form>
+      </Col>
+      <Col></Col>
+      </Row>
+      </Container>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import { restoreCSRF, csrfFetch } from './store/csrf';
