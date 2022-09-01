754861660fc866b7e4af64160d63091b1e17208c
Casey Cochran <Casey3362@yahoo.com>
2022-09-01 14:51:53 -0700

    login page styled with bootstrap

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { csrfFetch } from "../../store/csrf";
-import { Form, FormGroup, Label, Input, Container, Col, Row } from "reactstrap";
+import { Form, FormGroup, Label, Input, Container, Col, Row, Button } from "reactstrap";
+import './Signup.css';
 
 const Signup = () => {
   const [username, setUserName] = useState("");
@@ -27,35 +28,34 @@ const Signup = () => {
   };
 
   return (
-    <div >
-      <Container>
-        <Row>
-        <Col></Col>
-        <Col>
-      <Form className=".col-6 .col-sm-4"  onSubmit={handleSubmit}>
+    <div className="s-background">
+    <Container className="min-vh-100 f-center ">
+      <Form className="col-sm-6 form"  onSubmit={handleSubmit}>
+          <h1 className="text-center">Sign Up Here</h1>
         <FormGroup>
-        <div>
+        <div className="pb-2">
           <Label>Username</Label>
           <Input type="text" onChange={(e) => setUserName(e.target.value)} />
         </div>
-        <div>
+        </FormGroup>
+        <FormGroup>
+        <div className="pb-2">
           <Label>Email</Label>
           <Input type="text" onChange={(e) => setEmail(e.target.value)} />
         </div>
-        <div>
+        </FormGroup>
+        <FormGroup>
+        <div className="pb-2">
           <Label>Password</Label>
           <Input
             type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button>Submit</button>
         </FormGroup>
+        <Button color='primary'>Submit</Button>
       </Form>
-      </Col>
-      <Col></Col>
-      </Row>
-      </Container>
+    </Container>
     </div>
   );
 };
