0c5120cf3dc1f2be391e49f6c117a4eaf6dbbe2c
Casey Cochran <Casey3362@yahoo.com>
2022-09-03 17:45:03 -0700

    testing styling for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes} from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar.js';
 import Home from './components/Home/Home.js';
 import Login from './components/Login/Login.js';
 import Signup from './components/Signup/Signup.js';
 import Socket from './components/Socket/Socket';
 import { restoreUser } from './store/userSlice.js';
+import Navigation from './components/Navbar/Navbar.js';
 
 function App() {
   const dispatch = useDispatch();
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div >
-      <Navbar />
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
