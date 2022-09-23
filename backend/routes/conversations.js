19f945e6c530e41871893cacee9ee94bce9d1c85
Casey Cochran <Casey3362@yahoo.com>
2022-09-22 20:21:00 -0700

    adjusted conversation post route to trhow error if convoexists and user has same room, added delete route which is currently deleting document

--- backend/routes/conversations.js
+++ backend/routes/conversations.js
@@ -8,7 +8,7 @@ import { check } from "express-validator";
 
 const router = express.Router();
 
-//TODO add auth to these routes
+//Retrieve all ChatRooms based upon a users id
 router.get(
   "/:userId",
   requireAuth,
@@ -21,21 +21,17 @@ router.get(
   })
 );
 
-const validateNewConversation = [
-  check("username", "Please enter a valid email").isEmail(),
-  handleValidationErrors,
-];
-//TODO add auth and validation
-//Need to validate username, query user by that user name and get their id then create a conversation
-//Would be better to search user and click on their username which sends id
-//or is it the same because you would have to query twice either way with search
-//search for current convos and if current convo with both users send back error
+
+//Creates a new ChatRoom, if username does not exists it sends back err
+//Likewise if the ChatRoom already exists between the two same users it sends back error
 router.post(
   "/new",
   requireAuth,
   asyncHandler(async (req, res) => {
     const { username, userId } = req.body;
     const friend = await User.findOne({ username });
+    const userChatRoom = await ChatRoom.findOne({_id: userId})
+    console.log(userChatRoom, ' what is this, should be nothing')
     if (!friend) {
       return res.json({ err: "User does not exist" });
     }
@@ -43,14 +39,25 @@ router.post(
       members: friend._id.toString(),
       userId,
     });
-    if (convoAlreadyExists) {
+    if (convoAlreadyExists && userChatRoom) {
       return res.json({ err: "Conversation with this user already exists" });
     }
     const members = [friend._id.toString(), userId];
     const newChatRoom = new ChatRoom({ members, userId });
     newChatRoom.save();
-    res.json(newChatRoom);
+    //Added timestamps to model? for new room?
+    res.json({room: newChatRoom});
   })
 );
+
+//Deletes ChatRoom based upon UserId
+router.delete('/delete', requireAuth, asyncHandler(async(req,res) => {
+    const {convoId} = req.body;
+    const deleteRoom = await ChatRoom.deleteOne({_id: convoId});
+    console.log(deleteRoom.deletedCount === 1)
+}))
+
+
+
 
 export default router;
