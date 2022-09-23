1cdc9860062a1fd5dc2cb09218b7a5e7a35d39f4
Casey Cochran <Casey3362@yahoo.com>
2022-09-23 15:57:13 -0700

    working on creating chatroom logic to allow delete of room and recreation between separate users

--- backend/routes/conversations.js
+++ backend/routes/conversations.js
@@ -31,22 +31,25 @@ router.post(
     const { username, userId } = req.body;
     const friend = await User.findOne({ username });
     const userChatRoom = await ChatRoom.findOne({_id: userId})
-    console.log(userChatRoom, ' what is this, should be nothing')
+    console.log(userChatRoom, 'si this true or null')
     if (!friend) {
       return res.json({ err: "User does not exist" });
     }
     const convoAlreadyExists = await ChatRoom.find({
-      members: friend._id.toString(),
-      userId,
+      members: { $in: friend._id.toString(), $in: userId},
+
     });
-    if (convoAlreadyExists && userChatRoom) {
+    //if user is already apart of other convos but it is not his create chatroom
+    //then allow user to create new chatroom with that person, else throw error
+    if (convoAlreadyExists.length > 0 && !userChatRoom) {
       return res.json({ err: "Conversation with this user already exists" });
     }
+    console.log(friend, userId, ' should be diferent')
     const members = [friend._id.toString(), userId];
     const newChatRoom = new ChatRoom({ members, userId });
     newChatRoom.save();
     //Added timestamps to model? for new room?
-    res.json({room: newChatRoom});
+    res.json(newChatRoom);
   })
 );
 
@@ -55,6 +58,7 @@ router.delete('/delete', requireAuth, asyncHandler(async(req,res) => {
     const {convoId} = req.body;
     const deleteRoom = await ChatRoom.deleteOne({_id: convoId});
     console.log(deleteRoom.deletedCount === 1)
+    res.json({msg: 'ok'})
 }))
 
 
